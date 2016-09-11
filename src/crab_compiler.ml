d59a2a52ebf0df9c214afb48f01a438d3e4c1d7b
Heasummn <heasummn@Heasummn.T-mobile.com>
2016-09-10 20:48:11 -0500

    Seperated usage of Crab and the Build system

--- src/crab_compiler.ml
+++ src/crab_compiler.ml
@@ -3,6 +3,8 @@ open CrabSemantic
 open CrabCompile
 open Batteries.String
 
+let exit_status = ref 0
+
 let filename = Sys.argv.(1)
 let output = fst (rsplit filename ".") ^ ".o"
 
@@ -10,7 +12,8 @@ let dump_funcs = List.iter (fun x -> dump_val x)
 
 let main () = 
     let input = open_in filename in
-    try
+    begin
+        try
         let ctx = CrabEnv.base_ctx in
         let parsed = CrabParsing.process_chan input in
         let typed = annotateAST ctx parsed in
@@ -18,7 +21,9 @@ let main () =
         init_compiler;
         create_obj output CrabCodegen.glob_module;
     with 
-        | Error.SyntaxError (msg)   -> Printf.fprintf stderr "Syntax Error: %s\n" msg
-        | Error.ParsingError(msg)   -> Printf.fprintf stderr "Parsing Error: %s\n" msg
-        | Error.TypeError(msg)      -> Printf.fprintf stderr "Type Error: %s\n" msg
+        | Error.SyntaxError (msg)   -> Printf.fprintf stderr "Syntax Error: %s\n" msg; exit_status := 1;
+        | Error.ParsingError(msg)   -> Printf.fprintf stderr "Parsing Error: %s\n" msg; exit_status := 1;
+        | Error.TypeError(msg)      -> Printf.fprintf stderr "Type Error: %s\n" msg; exit_status := 1;
+    end;
+    exit !exit_status
 let _ = main ()
