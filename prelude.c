a568f30dff9704a1b6ec1cc9d420e04fa7d8f650
Heasummn <hassamuddin20@gmail.com>
2016-09-11 20:56:13 -0500

    Modify the prelude

--- prelude.c
+++ prelude.c
@@ -22,12 +22,13 @@ int get_int0()
 {
 	int ret, n;
 	char junk_chars[256];
+	
 	ret = scanf("%d", &n);
-	if(ret == EOF || ret != 1)
+	while(ret == EOF || ret != 1)
 	{
 		// Clear the buffer if we couldn't match.
-		scanf("%s", &junk_chars);
-		return 0;
+		scanf("%s\n", junk_chars);
+		ret = scanf("%d", &n);
 	}
 	return n;
 }
